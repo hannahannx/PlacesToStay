068e6636287871ce6e81f98c85047300bb07581f
hannahannx <4HACKH44@solent.ac.uk>
2024-03-21 16:30:31 +0000

    t1 - look up all acc given location

--- server.mjs
+++ server.mjs
@@ -8,6 +8,7 @@ const PORT = 3000
 const app = express();
 const db = new Database('placestostay.db')
 
+//home route - should render the index page for later on 
 app.get('/', (req,res)=> {
     res.send('Test');
 });
@@ -23,8 +24,18 @@ app.get('/test',(req,res)=>{
     }
 });
 
+//Task 1 - Look up all accommodation in a given location.
+app.get('/placestostay/accommodation/:location', (req,res) => {
+    try{
+        const smth = db.prepare(`SELECT * FROM accommodation WHERE location=? `)
+        const results = smth.all(req.params.location)
+        res.json(results)
+    }catch(error){
+        res.status(500).json({error:error})
+    }
+});
 
 
-//
+
 console.log("Web Application Listening at http://localhost:3000")
 app.listen(PORT);
