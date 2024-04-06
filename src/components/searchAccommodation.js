43ff6ada0da3a34b865e7b564fb8e25b390269e6
hannahannx <4HACKH44@solent.ac.uk>
2024-04-06 03:51:52 +0100

    completed react base proj - started t4

---
+++ src/components/searchAccommodation.js
@@ -0,0 +1,14 @@
+// searchAccommodation.js
+
+import React from "react";
+
+function Test(){
+    return  <div style = {{backgroundColor:'#586994'}}>
+                <h1>  
+                    This is the page for accommodation searching
+                </h1>
+            </div>
+    
+}
+
+export default Test
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Test from './components/searchAccommodation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World!</h1>);
+root.render(<Test></Test>);
