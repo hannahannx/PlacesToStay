c7ae38a6fbc66154b875672568756e3fb28966da
hannahannx <4HACKH44@solent.ac.uk>
2024-04-07 17:31:20 +0100

    t4 completed - searches successfully and created frontend

--- src/components/searchAccommodation.js
+++ src/components/searchAccommodation.js
@@ -2,13 +2,51 @@
 
 import React from "react";
 
-function Test(){
-    return  <div style = {{backgroundColor:'#586994'}}>
-                <h1>  
-                    This is the page for accommodation searching
-                </h1>
+function SearchAccomm(){
+
+    const [currentAccomm,setCurrentAccomm] = React.useState("");
+    const [searchResults,setSearchResults] = React.useState([]);
+
+
+    const allAccommodations = searchResults.map (accommodation =>
+        <p style= {{backgroundColor: '#7D869C',color: '#E5E8B6',textAlign:"center"}} key={accommodation.id}>
+        Name: {accommodation.name} <br></br>
+        Type: {accommodation.type} <br></br>
+        Latitude: {accommodation.latitude} <br></br>
+        Longitude: {accommodation.longitude} <br></br>
+        </p>
+        );
+
+    return  <div style = {{backgroundColor:'#586994',textAlign:"center"}}>
+                <h1 style= {{color: '#E5E8B6',textAlign:"center"}}>Places to Stay</h1>
+                <p style={{color:'#B4C4AE'}}>Please enter the location you would like to search for:</p>
+                <input type="button" value='Update Accommodation Name' onClick={updateAccommName}/>
+                <input placeholder="Locaation Name..." id="location"/>
+                <div style={{color: '#A2ABAB'}}>{currentAccomm}</div>
+                <br></br>
+                <input type="button" value='Search' onClick={displayAllLocation}/>
+                <br></br>
+                <br></br>
+                <h2 style= {{color: '#E5E8B6',textAlign:"center" }} >Results</h2>
+                <div id='locationSearchResults'>
+                    
+                        {allAccommodations}
+                    
+                </div>
+                <br></br>
             </div>
+
+function updateAccommName(){
+    setCurrentAccomm(document.getElementById('location').value);
+}
     
+async function displayAllLocation(){
+    
+    const response = await fetch (`http://localhost:3000/placestostay/accommodation/${currentAccomm}`)
+    const allAccommodations = await response.json();
+    console.log(JSON.stringify(allAccommodations))
+    setSearchResults(allAccommodations)
+}           
 }
 
-export default Test
+export default SearchAccomm
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//index.js 
+
+//imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Test from './components/searchAccommodation';
