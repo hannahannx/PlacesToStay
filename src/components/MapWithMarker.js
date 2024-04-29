c26bf64ff8ae2dd922635330af9b7f0c33d105a8
hannahannx <4HACKH44@solent.ac.uk>
2024-04-29 12:49:11 +0100

    t7 - works only if you input the values of the search results maually not automatically

--- src/components/MapWithMarker.js
+++ src/components/MapWithMarker.js
@@ -1,48 +1,41 @@
 import React from "react";
 import "leaflet";
+import allAccommodations from "./searchAccommodation";
 
 function Map({ lat1, lon1 }) {
     const map = React.useRef(null);
     const [lat, setLat] = React.useState(lat1 || 51.05);
     const [lon, setLon] = React.useState(lon1 || -0.72);
 
-    React.useEffect( ()=> {
+    React.useEffect(() => {
         // As effect will run only once, there is no need to check if map.current is null
         map.current = L.map("map1");
         // Set the map up in the normal way
         L.tileLayer
-        ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            { attribution: "Copyright OSM contributors, ODBL" } ).addTo(map.current);
-        const pos = [lat, lon];    
+            ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                { attribution: "Copyright OSM contributors, ODBL" }).addTo(map.current);
+        const pos = [lat, lon];
         map.current.setView(pos, 15);
         // Handle the map moveend event by updating the state appropriately
-        map.current.on("moveend", e=> {
+        map.current.on("moveend", e => {
             const centre = map.current.getCenter();
             setLat(centre.lat);
             setLon(centre.lng);
         });
-        map.current.on("click", e => {
-            // "e.latlng" is an object (of type L.LatLng) representing the mouse click 
-            // position
-            // It has two properties, "lat" is the latitude and "lng" is the longitude.
-            const marker = L.marker(pos).addTo(map.current);
-            marker.bindPopup("My Location");
-            alert(`You clicked at:${e.latlng.lat} ${e.latlng.lng}`);
-        });
-    },[]);
+    }, []);
 
 
-    return(
+    return (
         <div className="mainContentDiv">
-        <input id='lat' placeholder="Latitude..."/>
-        <input id='lon' placeholder="Longitude..."/>
-        <input type='button' value='go' onClick={setPos} />
-        <p>Map centred at: {lat} {lon}</p>
-        <div id="map1" style={{height:"500px", alignItems: "center", justifyContent: "center"}}></div>
+            <input id='lat' placeholder="Latitude..." />
+            <input id='lon' placeholder="Longitude..." />
+            <input type='button' value='go' onClick={setPos} />
+            <p>Map centred at: {lat} {lon}</p>
+            <div id="map1" style={{ height: "500px", alignItems: "center", justifyContent: "center" }}></div>
         </div>
     );
-    
-//for each item which is on the search accom list, place a marker with the infomation for that positon on the map
+
+    //for each item which is on the search accom list, place a marker with the infomation for that positon on the map
 
     function setPos() {
         const lt = document.getElementById('lat').value;
@@ -50,6 +43,8 @@ function Map({ lat1, lon1 }) {
         setLat(lt);
         setLon(lng);
         map.current.setView([lt, lng], 14);
+        const marker = L.marker([lt,lng]).addTo(map.current);
+        marker.bindPopup("My Location");
     }
 }
 
